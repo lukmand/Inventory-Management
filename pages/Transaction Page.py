954a5ec84a8bfa745e26bed8caf97a3d910b9ad3
lukmand <lukmand@users.noreply.github.com>
2023-03-16 10:19:31 +0700

    Update Transaction Page.py

--- pages/Transaction Page.py
+++ pages/Transaction Page.py
@@ -137,7 +137,7 @@ if submit:
         for i in st.session_state.transaction_df.values.tolist():
             data.gspread_write_data(gsheet_transaction, i)
         
-        data.gspread_write_data(gsheet_invoice, [invoiceid, datetime.datetime.now(), "PROCESS", vendor, invoiceprize])
+        data.gspread_write_data(gsheet_invoice, [invoiceid, datetime.datetime.now(), "PROCESS", vendor, invoiceprize, 0])
         st.session_state.transaction_df = st.session_state.transaction_df.iloc[0:0]
         #else:
         #    st.warning("item exceeds stocks")
