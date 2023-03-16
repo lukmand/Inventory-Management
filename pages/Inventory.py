6c8b8b4af16b45819b49c4189f1d0b4f42cb57c7
lukmand <lukmand@users.noreply.github.com>
2023-03-16 19:35:38 +0700

    Add files via upload

--- pages/Inventory.py
+++ pages/Inventory.py
@@ -1,7 +1,17 @@
 import streamlit as st
 import hashlib
 import data
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.write('''
-	Inventory Page
+	# Inventory Page
 ''')
+
+inventory_data = get_product(st.secrets["inventory_gsheets_url"])
+inventory_grid = AgGrid(inventory_data, editable=True, fit_columns_on_grid_load=True)
+save_inventory_button = st.button("Save Inventory Data")
+if save_inventory_button:
+    inventory_df = inventory_grid['data']
+    data.gspread_upload_data("Inventory", inventory_df)
+    st.success("Data is updated")
